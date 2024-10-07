c14c461d55f5fda18f9333972f7a9af4a45e31a2
Alwyn-Tan <2660412180@qq.com>
2024-10-07 21:32:39 +0800

    update： untracked files

---
+++ 灵茶/2653.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        int[] ans = new int[nums.length - k + 1];
+        for (int i = 0; i <= nums.length - k; i++) {
+            int[] arr = Arrays.copyOfRange(nums, i, i + k);
+            Arrays.sort(arr);
+            if (arr[x - 1] < 0) {
+                ans[i] = arr[x - 1];
+            } else {
+                ans[i] = 0;
+            }
+        }
+    }
+}
