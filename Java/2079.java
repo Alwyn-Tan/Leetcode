fdeafbe39caff7b2b81a6958723f43cc2a286d11
Alwyn-Tan <2660412180@qq.com>
2024-10-12 23:48:20 +0800

    fix: 2079

--- Java/2079.java
+++ Java/2079.java
@@ -1,16 +1,16 @@
 class Solution {
     public int wateringPlants(int[] plants, int capacity) {
         int ans = 0;
-        int start = -1;
         int curr = capacity;
         for (int i = 0; i < plants.length; i++) {
             if (curr >= plants[i]) {
                 curr -= plants[i];
                 ans++;
             } else {
-                curr = capacity;
-                ans += (i - start) * 2 + 1;
+                curr = capacity - plants[i];
+                ans += i  * 2 + 1;
             }
         }
+        return ans;
     }
 }
