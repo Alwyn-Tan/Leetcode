20205acbac4bf85dbae4e329f4917cdf1044a8d8
Alwyn-Tan <2660412180@qq.com>
2024-10-15 23:59:23 +0800

    fix: 0034

--- 灵茶/二分法/0034.java
+++ 灵茶/二分法/0034.java
@@ -1,46 +1,24 @@
 class Solution {
 
     public int[] searchRange(int[] nums, int target) {
-        return new int[] { findLeft(nums, target), findRight(nums, target) };
+        int start = findBound(nums, target);
+        if (start == nums.length || nums[start] != target) {
+            return new int[] { -1, -1 };
+        } else {
+            return new int[] { start, findBound(nums, target + 1) - 1 };
+        }
     }
 
-    public int findLeft(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length;
+    public int findBound(int[] nums, int target) {
+        int left = 0, right = nums.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
                 left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid;
             } else {
-                if (mid == 0 || nums[mid - 1] != target) {
-                    return mid;
-                } else {
-                    right = mid;
-                }
+                right = mid;
             }
         }
-        return -1;
-    }
-
-    public int findRight(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid;
-            } else {
-                if (mid == nums.length - 1 || nums[mid + 1] != target) {
-                    return mid;
-                } else {
-                    left = mid + 1;
-                }
-            }
-        }
-        return -1;
+        return left;
     }
 }
