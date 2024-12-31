6a082d88cba108edab4db3f57b182b0a039f5688
Alwyn-Tan <2660412180@qq.com>
2024-12-31 22:46:54 +0800

    add: 0416

---
+++ 灵茶/动态规划/背包/0416.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        int[] dp = new int[target + 1];
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+}
--- 灵茶/动态规划/背包/2915.java
+++ 灵茶/动态规划/背包/2915.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
-
+        int[] dp = new int[target + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum = Math.min(sum + nums.get(i), target);
+            for (int j = sum; j >= nums.get(i); j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums.get(i)] + 1);
+            }
+        }
+        int ans = dp[target];
+        return ans < 0 ? -1 : ans;
     }
 }
