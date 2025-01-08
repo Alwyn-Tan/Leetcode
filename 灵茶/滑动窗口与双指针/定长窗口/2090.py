fe65b287e7d4f7a8760b0503d910ff9038a3058b
Alwyn-Tan <2660412180@qq.com>
2025-01-08 21:36:22 +0800

    fix: 2909

--- 灵茶/滑动窗口与双指针/定长窗口/2090.py
+++ 灵茶/滑动窗口与双指针/定长窗口/2090.py
@@ -1,3 +1,20 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        List[int] 
+        if k ==0:
+            return nums
+        sum = 0
+        ans = [0] * len(nums)
+        
+        for index, num in enumerate(nums):
+            sum += num
+            if index >= 2 * k:
+                ans[index - k] = int (sum / (2 * k + 1))
+                sum -= nums[index - 2 * k]
+            ans[index] = -1
+
+
+        return ans
+
+
+
+        
