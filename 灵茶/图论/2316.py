95d05dbf6241735680fbcdf8eca6bedaf59462dc
Alwyn-Tan <2660412180@qq.com>
2025-01-12 16:35:23 +0800

    fix: 2316.py

--- 灵茶/图论/2316.py
+++ 灵茶/图论/2316.py
@@ -1,9 +1,9 @@
 class UnionFind():
-    def __init__(self, n):
-        self.S = [ i for i in range(0, n)]
+    def __init__(self, n: int):
+        self.S = [i for i in range(n)]
         self.size = [1] * n
 
-    def find(self, x:int):
+    def find(self, x:int) -> int:
         if self.S[x] != x:
             self.S[x] = self.find(self.S[x])
         return self.S[x]
@@ -13,16 +13,17 @@ class UnionFind():
 
         if x_root != y_root:
             self.S[x_root] = y_root
-            self.size[self.S[y_root]] += self.size(self.S[x_root])
+            self.size[self.S[y_root]] += self.size[self.S[x_root]]
     
-    def get_size(self, x: int):
+    def get_size(self, x: int) -> int:
         return self.size[x]
 
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
         uf = UnionFind(n)
-        for edge in enumerate(edges):
-            uf.union(edge[0], edge[1])
+        result = 0
+        for x, y in edges:
+            uf.union(x, y)
         for i in range (0, n):
             result += n - uf.get_size(uf.find(i))
         
